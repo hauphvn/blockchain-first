9fd25cc594036c9ae7e995d15ecac59ad4744112
tonyc_ubuntu_vm <hauphvn@gmail.com>
2021-08-28 23:31:26 +0700

    hash algorithrm

---
+++ cryto/block.js
@@ -0,0 +1,22 @@
+const GENESIS = require('./config');
+class Block {
+    constructor({timestamp, lastHash, data, hash}){
+        this.timestamp = timestamp;
+        this.lastHash = lastHash;
+        this.data = data;
+        this.hash = hash;
+    }
+
+    static genesis(){
+        return new this(GENESIS);
+    }
+    static mineBlock({lastBlock, data}) {
+        return new this({
+            timestamp: Date.now(),
+            lastHash: lastBlock.hash,
+            data
+        });
+    }
+}
+
+module.exports = Block;
---
+++ cryto/crypto-hash.test.js
@@ -0,0 +1,8 @@
+const  cryptoHash = require('./crypto-hash');
+
+describe('cryptoHash()', () => {
+    it('generates a SHA-256 hashed output',  () => {
+        expect(cryptoHash('foo'))
+            .toEqual('2C26B46B68FFC68FF99B453C1D30413413422D706483BFA0F98A5E886266E7AE');
+    });
+});
